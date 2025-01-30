117162f846fd3734436d7b77dcc0ef4d5da517b4
elanealencar <elanealencar@gmail.com>
2025-01-30 09:53:22 -0300

    responsividade ao menu

--- src/components/Menu/Menu copy teste.tsx
+++ src/components/Menu/Menu copy teste.tsx
@@ -1,24 +1,13 @@
-import { useState } from 'react';
 import './Menu.css';
 
 
 const Menu = () => {
-
-  const [menuOpen, setMenuOpen] = useState(false);
-
+  
   return (
     <nav className="menu">
-      <div className="menu-left">&lt; Elane Alencar /&gt;</div>
+        <div className="menu-left">&lt; Elane Alencar /&gt;</div>
 
-      <button
-        className="hamburger"
-        aria-label="Abrir menu"
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        ☰
-      </button>
-
-        <ul className={`menu-right ${menuOpen ? "open" : ""}`}>
+        <ul className="menu-right">
             <li><a href="#initial">Início</a></li>
             <li><a href="#about-section">Sobre Mim</a></li>
             <li><a href="#habilities-section">Habilidades</a></li>
