937e4b68323aebacd19a83bd9300436ac3df7292
elanealencar <elanealencar@gmail.com>
2025-01-30 10:27:09 -0300

    responsividade à seção aboutl

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,14 +1,14 @@
 import './About.css';
-import iconname from '../../assets/icons/icon-name.svg';
-import iconinstagram from '../../assets/icons/icon-instagram.svg';
-import iconlinkedin from '../../assets/icons/icon-linkedin.svg';
-import imageelane from '../../assets/images/image-elane.png';
+import iconName from '../../assets/icons/icon-name.svg';
+import iconInstagram from '../../assets/icons/icon-instagram.svg';
+import iconLinkedin from '../../assets/icons/icon-linkedin.svg';
+import imageElane from '../../assets/images/image-elane.png';
 
 
 
 const About = () => {
     return (
-        <div className="about-section" id="about-section">
+        <section className="about-section" id="about-section">
             <h1>Sobre Mim</h1>
             <div className="about-text">
                 <p>Possuo experiência em desenvolvimento web e mobile.</p> 
@@ -22,30 +22,29 @@ const About = () => {
             </div>
             <div className="about-contact">
                 <div className="about-contact-image">
-                    <img src={imageelane}></img>
+                    <img src={imageElane} alt="Elane Alencar" />
                 </div>
                 
                 <div className="about-contact-item">
-                    <img src={iconname}></img>
+                    <img src={iconName} alt="Ícone de Nome" />
                     <h3>Nome</h3>
                     <div>Elane Alencar</div>
                 </div>
                 
                 <div className="about-contact-item">
-                    <img src={iconinstagram}></img>
+                    <img src={iconInstagram} alt="Ícone do Instagram" />
                     <h3>Instagram</h3>
                     <div>@dev.elane</div>
                 </div>
                 <div className="about-contact-item">
-                    <a href='https://linkedin.com/in/elanealencar' target="_blank">
-                        <img src={iconlinkedin}></img> 
+                    <a href='https://linkedin.com/in/elanealencar' target="_blank" rel="noopener noreferrer">
+                        <img src={iconLinkedin} alt="Ícone do LinkedIn" />
                         <h3>Linkedin</h3>
                         <div>elanealencar</div>
                     </a>
-
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
