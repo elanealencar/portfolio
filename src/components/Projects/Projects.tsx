4e2b739a5e041c9465778a356ceddd7ee008def2
elanealencar <elanealencar@gmail.com>
2025-04-26 21:36:43 -0300

    update projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,44 +1,52 @@
 import './Projects.css';
-import project01 from '../../assets/images/portfolio.png';
-import project02 from '../../assets/images/imc.png';
-import project03 from '../../assets/images/gerador-numeros-da-sorte.png';
-import project04 from '../../assets/images/performance.png';
-import project05 from '../../assets/images/sorteador.png'
+import previewPortfolio from '../../assets/images/portfolio.png';
+import previewImc from '../../assets/images/imc.png';
+import previewNumeros from '../../assets/images/gerador-numeros-da-sorte.png';
+import previewPerformance from '../../assets/images/performance.png';
+import previewSorteador from '../../assets/images/sorteador.png'
+import previewAvanti from '../../assets/images/avanti_desktop.png'
 import ProjectCard from './ProjectCard';
 
 const Projects = () => {
 
     const projectsData = [
         {
-            img: project01,
+            img: previewPortfolio,
             title: "Portfólio pessoal",
             tech: "HTML | CSS | JavaScript | React | Vite",
             projectLink: "https://portfolio-elanealencar.vercel.app/",
             repoLink: "https://github.com/elanealencar/portfolio/",
           },
           {
-            img: project02,
+            img: previewImc,
             title: "Calculadora de IMC",
             tech: "HTML | CSS Modules | Typescript | React | Vite",
             projectLink: "https://calc-imc-theta.vercel.app",
             repoLink: "https://github.com/elanealencar/calc-imc",
           },
           {
-            img: project04,
+            img: previewAvanti,
+            title: "Avanti - Loja de E-commerce",
+            tech: "HTML | Javascript | Tailwind CSS",
+            projectLink: "https://avanti-landing-page.vercel.app/",
+            repoLink: "https://github.com/elanealencar/avanti-landing-page"
+          }
+          {
+            img: previewPerformance,
             title: "Performance - Site Institucional",
             tech: "HTML | CSS | Javascript",
             projectLink: "https://performanceconsultoria.com/",
             repoLink: "https://github.com/elanealencar/performance",
           },
           {
-            img: project05,
+            img: previewSorteador,
             title: "Sorteador de Duplas para Campeonato de Futevôlei",
             tech: "HTML | Tailwind CSS | Javascript",
             projectLink: "https://sorteio-campeonato.vercel.app/",
             repoLink: "https://github.com/elanealencar/sorteio-campeonato",
           },
           {
-            img: project03,
+            img: previewNumeros,
             title: "Gerador de números da sorte",
             tech: "HTML | CSS | Javascript",
             projectLink: "https://gerador-de-numeros-da-sorte.vercel.app",
